82dd6b17ccff6e0ddce4aba667277254ee56de55
Dustin Haffner <dh@xix.org>
2020-08-06 10:33:55 -0700

    Fix static folder relative path

--- cubie/server.py
+++ cubie/server.py
@@ -77,8 +77,13 @@ schema = make_executable_schema(type_defs, query, cubeType)
 
 app = Flask(
     __name__,
-    static_folder='./build',
+    static_folder='../build',
     static_url_path='/')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route("/graphql", methods=["GET"])
