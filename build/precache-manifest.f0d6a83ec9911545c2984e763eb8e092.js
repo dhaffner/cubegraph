8c679feff1f32ca5b1a82e9d0c90eb795cec4402
Dustin Haffner <dh@xix.org>
2020-08-06 12:19:46 -0700

    Update frontend build

---
+++ build/precache-manifest.f0d6a83ec9911545c2984e763eb8e092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d52c5164ddd52ef668405113a45952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea1549a668ebcb832c0",
+    "url": "/static/css/main.d14094fe.chunk.css"
+  },
+  {
+    "revision": "531962a73ffb8577ae48",
+    "url": "/static/js/2.fe91e63a.chunk.js"
+  },
+  {
+    "revision": "2db57e0c047beb4bf47770050fcde478",
+    "url": "/static/js/2.fe91e63a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea1549a668ebcb832c0",
+    "url": "/static/js/main.94121cf4.chunk.js"
+  },
+  {
+    "revision": "75cae36096806cccda0a",
+    "url": "/static/js/runtime-main.2becc1bc.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf1078f252035e457fc3fb4032e53c21.js"
+  "/precache-manifest.f0d6a83ec9911545c2984e763eb8e092.js"
 );
 
 self.addEventListener('message', (event) => {
